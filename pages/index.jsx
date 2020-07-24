e0f92bb8e2bf2b08d49934959fe45b5a7d55ce52
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2020-07-23 20:10:43 -0500

    basics

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import styled from '@emotion/styled';
+import Layout from '../components/layouts/Layout';
+
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Layout>
+        <h1>hey</h1>
+      </Layout>
+    </div>
+  )
+}
---
+++ pages/we.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import styled from '@emotion/styled';
+import Layout from '../components/layouts/Layout';
+
+
+export default function We() {
+  return (
+    <div className="container">
+      <Layout>
+        <h1>About</h1>
+      </Layout>
+    </div>
+  )
+}
